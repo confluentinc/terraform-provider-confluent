c880346c4449f6630a225df78112e7103632fcac
zhenli00 <120421969+zhenli00@users.noreply.github.com>
2023-01-18 20:12:01 -0800

    SVCF-3455: add Postgres source cdc connector example (#331)
    * postgres source connector
    
    * fmt
    
    * change to equal sign for assignment
    
    * rename folder

---
+++ examples/configurations/postgre-sql-cdc-debezium-source-connector/main.tf
@@ -0,0 +1,259 @@
+terraform {
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.24.0"
+    }
+  }
+}
+
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
+resource "confluent_environment" "staging" {
+  display_name = "Staging"
+}
+
+# Stream Governance and Kafka clusters can be in different regions as well as different cloud providers,
+# but you should to place both in the same cloud and region to restrict the fault isolation boundary.
+data "confluent_schema_registry_region" "essentials" {
+  cloud   = "AWS"
+  region  = "us-east-1"
+  package = "ADVANCED"
+}
+
+resource "confluent_schema_registry_cluster" "essentials" {
+  package = data.confluent_schema_registry_region.essentials.package
+
+  environment {
+    id = confluent_environment.staging.id
+  }
+
+  region {
+    # See https://docs.confluent.io/cloud/current/stream-governance/packages.html#stream-governance-regions
+    id = data.confluent_schema_registry_region.essentials.id
+  }
+}
+
+# Update the config to use a cloud provider and region of your choice.
+# https://registry.terraform.io/providers/confluentinc/confluent/latest/docs/resources/confluent_kafka_cluster
+resource "confluent_kafka_cluster" "basic" {
+  display_name = "inventory"
+  availability = "SINGLE_ZONE"
+  cloud        = "AWS"
+  region       = "us-east-1"
+  basic {}
+  environment {
+    id = confluent_environment.staging.id
+  }
+}
+
+resource "confluent_kafka_topic" "orders" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  topic_name    = "orders"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+// 'app-manager' service account is required in this configuration to create 'orders' topic and grant ACLs
+// to 'app-producer' and 'app-consumer' service accounts.
+resource "confluent_service_account" "app-manager" {
+  display_name = "app-manager"
+  description  = "Service account to manage 'inventory' Kafka cluster"
+}
+
+resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.app-manager.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.basic.rbac_crn
+}
+
+resource "confluent_api_key" "app-manager-kafka-api-key" {
+  display_name = "app-manager-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-manager' service account"
+  owner {
+    id          = confluent_service_account.app-manager.id
+    api_version = confluent_service_account.app-manager.api_version
+    kind        = confluent_service_account.app-manager.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+
+  # The goal is to ensure that confluent_role_binding.app-manager-kafka-cluster-admin is created before
+  # confluent_api_key.app-manager-kafka-api-key is used to create instances of
+  # confluent_kafka_topic, confluent_kafka_acl resources.
+
+  # 'depends_on' meta-argument is specified in confluent_api_key.app-manager-kafka-api-key to avoid having
+  # multiple copies of this definition in the configuration which would happen if we specify it in
+  # confluent_kafka_topic, confluent_kafka_acl resources instead.
+  depends_on = [
+    confluent_role_binding.app-manager-kafka-cluster-admin
+  ]
+}
+
+resource "confluent_service_account" "app-consumer" {
+  display_name = "app-consumer"
+  description  = "Service account to consume from 'orders' topic of 'inventory' Kafka cluster"
+}
+
+resource "confluent_service_account" "app-connector" {
+  display_name = "app-connector"
+  description  = "Service account of postgre sql db Source Connector to consume from 'orders' topic of 'inventory' Kafka cluster"
+}
+
+resource "confluent_api_key" "app-consumer-kafka-api-key" {
+  display_name = "app-consumer-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-consumer' service account"
+  owner {
+    id          = confluent_service_account.app-consumer.id
+    api_version = confluent_service_account.app-consumer.api_version
+    kind        = confluent_service_account.app-consumer.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+}
+
+resource "confluent_service_account" "app-producer" {
+  display_name = "app-producer"
+  description  = "Service account to produce to 'orders' topic of 'inventory' Kafka cluster"
+}
+
+resource "confluent_api_key" "app-producer-kafka-api-key" {
+  display_name = "app-producer-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-producer' service account"
+  owner {
+    id          = confluent_service_account.app-producer.id
+    api_version = confluent_service_account.app-producer.api_version
+    kind        = confluent_service_account.app-producer.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+}
+
+
+resource "confluent_kafka_acl" "app-connector-describe-on-cluster" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  resource_type = "CLUSTER"
+  resource_name = "kafka-cluster"
+  pattern_type  = "LITERAL"
+  principal     = "User:${confluent_service_account.app-connector.id}"
+  host          = "*"
+  operation     = "DESCRIBE"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+resource "confluent_kafka_acl" "app-connector-write-on-prefix-topics" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  resource_type = "TOPIC"
+  resource_name = local.database_server_name
+  pattern_type  = "PREFIXED"
+  principal     = "User:${confluent_service_account.app-connector.id}"
+  host          = "*"
+  operation     = "WRITE"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+resource "confluent_kafka_acl" "app-connector-create-on-prefix-topics" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  resource_type = "TOPIC"
+  resource_name = local.database_server_name
+  pattern_type  = "PREFIXED"
+  principal     = "User:${confluent_service_account.app-connector.id}"
+  host          = "*"
+  operation     = "CREATE"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+resource "confluent_connector" "postgre-sql-cdc-source" {
+  environment {
+    id = confluent_environment.staging.id
+  }
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+
+  // Block for custom *sensitive* configuration properties that are labelled with "Type: password" under "Configuration Properties" section in the docs:
+  // https://docs.confluent.io/cloud/current/connectors/cc-postgresql-cdc-source-debezium.html#configuration-properties
+  config_sensitive = {
+    "database.password" = "***REDACTED***"
+  }
+
+  // Block for custom *nonsensitive* configuration properties that are *not* labelled with "Type: password" under "Configuration Properties" section in the docs:
+  // https://docs.confluent.io/cloud/current/connectors/cc-postgresql-cdc-source-debezium.html#configuration-properties
+  config_nonsensitive = {
+    "connector.class"          = "PostgresCdcSource"
+    "name"                     = "PostgresCdcSourceConnector_0"
+    "kafka.auth.mode"          = "SERVICE_ACCOUNT"
+    "kafka.service.account.id" = confluent_service_account.app-connector.id
+    "database.hostname"        = "post0115.postgres.database.azure.com"
+    "database.port"            = "5432"
+    "database.user"            = "confluentuser"
+    "database.dbname"          = "test"
+    "database.server.name"     = local.database_server_name
+    "plugin.name"              = "pgoutput",
+    "output.data.format"       = "JSON",
+    "tasks.max"                = "1"
+  }
+
+  depends_on = [
+    confluent_kafka_acl.app-connector-describe-on-cluster,
+    confluent_kafka_acl.app-connector-write-on-prefix-topics,
+    confluent_kafka_acl.app-connector-create-on-prefix-topics,
+  ]
+}
+
+locals {
+  database_server_name = "sql"
+}
---
+++ examples/configurations/postgre-sql-cdc-debezium-source-connector/variables.tf
@@ -0,0 +1,10 @@
+variable "confluent_cloud_api_key" {
+  description = "Confluent Cloud API Key (also referred as Cloud API ID)"
+  type        = string
+}
+
+variable "confluent_cloud_api_secret" {
+  description = "Confluent Cloud API Secret"
+  type        = string
+  sensitive   = true
+}
