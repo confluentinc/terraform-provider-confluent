1d63b5ee49da1c8897a96b3342bcb1b629cdd28b
Channing Dong <165202380+channingdong@users.noreply.github.com>
2024-08-07 15:07:25 -0700

    #minor: Prepare for 1.83.0 release (#3)
    * Prepare for v1.83.0 minor release
    
    * Address all the PR review nits

--- examples/configurations/dedicated-transit-gateway-attachment-aws-kafka-rbac/main.tf
+++ examples/configurations/dedicated-transit-gateway-attachment-aws-kafka-rbac/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     confluent = {
       source  = "confluentinc/confluent"
-      version = "1.82.0"
+      version = "1.83.0"
     }
     aws = {
       source  = "hashicorp/aws"
