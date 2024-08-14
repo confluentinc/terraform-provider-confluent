02d92ebe9c066c7dfa4d515b1f2bbcff577f673a
Channing Dong <165202380+channingdong@users.noreply.github.com>
2024-08-14 11:00:13 -0700

    #major: Prepare for 2.0.0 release (#9)
    * #major: Prepare for 2.0.0 release
    
    * Update CHANGELOG.md per Kostya's suggestion
    
    Co-authored-by: Kostya Linou <klinou@confluent.io>
    
    * Update CHANGELOG.md
    
    Co-authored-by: Kostya Linou <klinou@confluent.io>
    
    * Update CHANGELOG.md typo
    
    ---------
    
    Co-authored-by: Kostya Linou <klinou@confluent.io>

--- examples/configurations/enterprise-privatelinkattachment-aws-kafka-acls/main.tf
+++ examples/configurations/enterprise-privatelinkattachment-aws-kafka-acls/main.tf
@@ -7,7 +7,7 @@ terraform {
     }
     confluent = {
       source  = "confluentinc/confluent"
-      version = "1.83.0"
+      version = "2.0.0"
     }
   }
 }
--- examples/configurations/flink-quickstart-2/main.tf
+++ examples/configurations/flink-quickstart-2/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     confluent = {
       source  = "confluentinc/confluent"
-      version = "1.83.0"
+      version = "2.0.0"
     }
   }
 }
