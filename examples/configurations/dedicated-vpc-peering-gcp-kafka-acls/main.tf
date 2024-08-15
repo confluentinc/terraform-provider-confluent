497196e96ebddbe01a58014d88c660cfd48e82c1
Channing Dong <165202380+channingdong@users.noreply.github.com>
2024-08-15 14:42:55 -0700

    #major: Prepare for 2.0.0 release (#9) (#417)
    * #major: Prepare for 2.0.0 release
    ---------
    Co-authored-by: Kostya Linou <klinou@confluent.io>

--- examples/configurations/dedicated-vpc-peering-gcp-kafka-acls/main.tf
+++ examples/configurations/dedicated-vpc-peering-gcp-kafka-acls/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     confluent = {
       source  = "confluentinc/confluent"
-      version = "1.83.0"
+      version = "2.0.0"
     }
     google = {
       source  = "hashicorp/google"
