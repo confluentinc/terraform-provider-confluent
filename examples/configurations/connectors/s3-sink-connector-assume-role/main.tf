e8ffc5269225f293046b5454732f497fcb0d1acf
Kostya Linou <klinou@confluent.io>
2024-10-27 22:13:52 -0700

    add s3_access_policy

--- examples/configurations/connectors/s3-sink-connector-assume-role/main.tf
+++ examples/configurations/connectors/s3-sink-connector-assume-role/main.tf
@@ -424,3 +424,42 @@ resource "confluent_connector" "s3-sink" {
     confluent_kafka_acl.app-connector-read-on-connect-lcc-group,
   ]
 }
+
+# https://docs.confluent.io/cloud/current/connectors/cc-s3-sink/cc-s3-sink.html#user-account-iam-policy
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "S3AccessPolicy"
+  description = "IAM policy for accessing the S3 bucket for Amazon S3 Sink Connector"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:ListAllMyBuckets"
+        ]
+        Resource = "arn:aws:s3:::*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:ListBucket",
+          "s3:GetBucketLocation",
+          "s3:ListBucketMultipartUploads"
+        ]
+        Resource = "arn:aws:s3:::${var.s3_bucket_name}"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:PutObject",
+          "s3:PutObjectTagging",
+          "s3:GetObject",
+          "s3:AbortMultipartUpload",
+          "s3:ListMultipartUploadParts"
+        ]
+        Resource = "arn:aws:s3:::${var.s3_bucket_name}/*"
+      }
+    ]
+  })
+}
