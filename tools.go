39f0ecca64a84ad39efb80eaf994507e78c0dd45
Stan <stan@confluent.io>
2024-12-04 13:36:50 -0800

    remove pact tests

--- tools.go
+++
@@ -1,8 +0,0 @@
-//go:build tools
-
-package main
-
-// This import is required to make sure go mod tidy doesn't remove packages that make pact-install depends on
-import (
-	_ "github.com/pact-foundation/pact-go/v2"
-)
