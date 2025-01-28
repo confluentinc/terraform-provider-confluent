e31cad751f03527438e56c17f9d7d08d6a34bad3
Kostya Linou <klinou@confluent.io>
2025-01-28 10:13:18 -0800

    Remove CustomizeDiff: customdiff.Sequence(resourceApiKeyManagedResourceDiff).

--- internal/provider/resource_api_key.go
+++ internal/provider/resource_api_key.go
@@ -21,7 +21,6 @@ import (
 	apikeys "github.com/confluentinc/ccloud-sdk-go-v2/apikeys/v2"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/customdiff"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"net/http"
@@ -99,7 +98,7 @@ func apiKeyResource() *schema.Resource {
 				ForceNew: true,
 			},
 		},
-		CustomizeDiff: customdiff.Sequence(resourceApiKeyManagedResourceDiff),
+		//CustomizeDiff: customdiff.Sequence(resourceApiKeyManagedResourceDiff),
 	}
 }
 
