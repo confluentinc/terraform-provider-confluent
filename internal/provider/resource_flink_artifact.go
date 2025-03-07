47634330aab77839a6883fb0c409d0417444ca8e
Daniel Ayaz <dayaz@confluent.io>
2025-03-07 10:38:53 -0600

    Added check for flinkudf

--- internal/provider/resource_flink_artifact.go
+++ internal/provider/resource_flink_artifact.go
@@ -156,7 +156,7 @@ func artifactCreate(ctx context.Context, d *schema.ResourceData, meta interface{
 		return diag.Errorf("error uploading Flink Artifact: error fetching presigned upload URL %s", createDescriptiveError(err))
 	}
 
-	if err := uploadFile(resp.GetUploadUrl(), artifactFile, resp.GetUploadFormData(), resp.GetContentFormat(), cloud); err != nil {
+	if err := uploadFile(resp.GetUploadUrl(), artifactFile, resp.GetUploadFormData(), resp.GetContentFormat(), cloud, true); err != nil {
 		return diag.Errorf("error uploading Flink Artifact: %s", createDescriptiveError(err))
 	}
 
