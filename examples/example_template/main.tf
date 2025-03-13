e7b09eefed43121e796d97612a87f1dd86f7811c
Tushar Malik <tmalik@confluent.io>
2025-03-13 12:46:00 -0500

    typo

--- examples/example_template/main.tf
+++ examples/example_template/main.tf
@@ -11,7 +11,7 @@ provider "confluent" {
     cloud_api_key         = var.confluent_cloud_api_key
     cloud_api_secret      = var.confluent_cloud_api_secret
     endpoint = "https://api.stag.cpdev.cloud" # for staging environment
-    # endpoint = "https://api.stag.cpdev.cloud" # for devel environment
+    # endpoint = "https://api.devel.cpdev.cloud" # for devel environment
 }
 
 # Add your resources/data sources here
