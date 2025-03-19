79831d1c26569d79bfb3c4e86d5a06ad64f41916
Kostya Linou <klinou@confluent.io>
2025-03-18 22:02:57 -0700

    add step 3 resources

--- examples/configurations/tableflow/datagen-connector-byob-aws-storage/outputs.tf
+++ examples/configurations/tableflow/datagen-connector-byob-aws-storage/outputs.tf
@@ -1,7 +1,10 @@
 output "resource-ids" {
   value = <<-EOT
-  Environment ID:   ${confluent_environment.staging.id}
+  You have completed steps #1-2 from https://docs.confluent.io/cloud/current/topics/tableflow/get-started/quick-start-managed-storage.html#step-1-create-a-topic-and-publish-data
+
+  Environment ID:   ${data.confluent_environment.staging.id}
   Kafka Cluster ID: ${confluent_kafka_cluster.basic.id}
+  Kafka Cluster Name: ${confluent_kafka_cluster.basic.display_name}
   Kafka topic name: ${confluent_kafka_topic.stock-trades.topic_name}
 
   Service Accounts and their Kafka API Keys (API Keys inherit the permissions granted to the owner):
@@ -17,42 +20,15 @@ output "resource-ids" {
   ${confluent_service_account.app-consumer.display_name}'s Kafka API Key:    "${confluent_api_key.app-consumer-kafka-api-key.id}"
   ${confluent_service_account.app-consumer.display_name}'s Kafka API Secret: "${confluent_api_key.app-consumer-kafka-api-key.secret}"
 
-  In order to use the Confluent CLI v2 to produce and consume messages from topic '${confluent_kafka_topic.stock-trades.topic_name}' using Kafka API Keys
-  of ${confluent_service_account.app-producer.display_name} and ${confluent_service_account.app-consumer.display_name} service accounts
-  run the following commands:
+  Step 3: Set up access to the Iceberg REST Catalog:
 
-  # 1. Log in to Confluent Cloud
-  $ confluent login
+  REST Catalog Endpoint: "https://tableflow.${var.aws_region}.aws.confluent.cloud/iceberg/catalog/organizations/${data.confluent_organization.main.id}/environments/${data.confluent_environment.staging.id}"
 
-  # 2. Produce key-value records to topic '${confluent_kafka_topic.stock-trades.topic_name}' by using ${confluent_service_account.app-producer.display_name}'s Kafka API Key
-  $ confluent kafka topic produce ${confluent_kafka_topic.stock-trades.topic_name} \
-        --schema "schemas/avro/purchase.avsc" \
-        --value-format avro \
-        --sr-endpoint ${data.confluent_schema_registry_cluster.essentials.rest_endpoint} \
-        --sr-api-key "${confluent_api_key.env-manager-schema-registry-api-key.id}" \
-        --sr-api-secret "${confluent_api_key.env-manager-schema-registry-api-key.secret}" \
-        --cluster ${confluent_kafka_cluster.basic.id} \
-        --api-key "${confluent_api_key.app-producer-kafka-api-key.id}" \
-        --api-secret "${confluent_api_key.app-producer-kafka-api-key.secret}" \
-        --environment ${confluent_environment.staging.id}
-  # Enter a few records and then press 'Ctrl-C' when you're done.
-  # Sample records:
-  # {"item":"pizza","amount":0.99,"customer_id":"lombardi"}
-  # {"item":"pizza","amount":1.99,"customer_id":"lombardi"}
-  # {"item":"ball","amount":29.99,"customer_id":"spalding"}
+  Service Account with "ResourceOwner" role for the scope=Cluster and its Tableflow API Key (API Keys inherit the permissions granted to the owner):
+  ${confluent_service_account.app-reader.display_name}:                        ${confluent_service_account.app-reader.id}
+  ${confluent_service_account.app-reader.display_name}'s Tableflow API Key:    "${confluent_api_key.app-reader-tableflow-api-key.id}"
+  ${confluent_service_account.app-reader.display_name}'s Tableflow API Secret: "${confluent_api_key.app-reader-tableflow-api-key.secret}"
 
-  # 3. Consume records from topic '${confluent_kafka_topic.stock-trades.topic_name}' by using ${confluent_service_account.app-consumer.display_name}'s Kafka API Key
-  $ confluent kafka topic consume purchase \
-        --from-beginning \
-        --value-format avro \
-        --sr-endpoint ${data.confluent_schema_registry_cluster.essentials.rest_endpoint} \
-        --sr-api-key "${confluent_api_key.env-manager-schema-registry-api-key.id}" \
-        --sr-api-secret "${confluent_api_key.env-manager-schema-registry-api-key.secret}" \
-        --cluster ${confluent_kafka_cluster.basic.id} \
-        --api-key "${confluent_api_key.app-consumer-kafka-api-key.id}" \
-        --api-secret "${confluent_api_key.app-consumer-kafka-api-key.secret}" \
-        --environment ${confluent_environment.staging.id}
-  # When you are done, press 'Ctrl-C'.
   EOT
 
   sensitive = true
