ab19e7f7c076de795372770e1de73c75ddab5d40
Kostya Linou <klinou@confluent.io>
2025-03-18 21:05:55 -0700

    remove SR resources

--- examples/configurations/tableflow/datagen-connector-byob-aws-storage/main.tf
+++ examples/configurations/tableflow/datagen-connector-byob-aws-storage/main.tf
@@ -228,48 +228,6 @@ resource "confluent_kafka_acl" "app-consumer-read-on-group" {
   }
 }
 
-resource "confluent_service_account" "env-manager" {
-  display_name = "env-manager"
-  description  = "Service account to manage 'Staging' environment"
-}
-
-resource "confluent_role_binding" "env-manager-environment-admin" {
-  principal   = "User:${confluent_service_account.env-manager.id}"
-  role_name   = "EnvironmentAdmin"
-  crn_pattern = confluent_environment.staging.resource_name
-}
-
-resource "confluent_api_key" "env-manager-schema-registry-api-key" {
-  display_name = "env-manager-schema-registry-api-key"
-  description  = "Schema Registry API Key that is owned by 'env-manager' service account"
-  owner {
-    id          = confluent_service_account.env-manager.id
-    api_version = confluent_service_account.env-manager.api_version
-    kind        = confluent_service_account.env-manager.kind
-  }
-
-  managed_resource {
-    id          = data.confluent_schema_registry_cluster.essentials.id
-    api_version = data.confluent_schema_registry_cluster.essentials.api_version
-    kind        = data.confluent_schema_registry_cluster.essentials.kind
-
-    environment {
-      id = confluent_environment.staging.id
-    }
-  }
-
-  # The goal is to ensure that confluent_role_binding.env-manager-environment-admin is created before
-  # confluent_api_key.env-manager-schema-registry-api-key is used to create instances of
-  # confluent_schema resources.
-
-  # 'depends_on' meta-argument is specified in confluent_api_key.env-manager-schema-registry-api-key to avoid having
-  # multiple copies of this definition in the configuration which would happen if we specify it in
-  # confluent_schema resources instead.
-  depends_on = [
-    confluent_role_binding.env-manager-environment-admin
-  ]
-}
-
 resource "confluent_api_key" "app-manager-tableflow-api-key" {
   display_name = "app-manager-tableflow-api-key"
   description  = "Tableflow API Key that is owned by 'app-manager' service account"
