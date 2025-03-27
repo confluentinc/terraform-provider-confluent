5080fb7ac8a0579fd5b7949432da3b7b57c4a7d9
Steven Gagniere <108363707+sgagniere@users.noreply.github.com>
2025-03-27 15:38:25 -0700

    #minor: prepare for v2.23.0 minor release (EXP-15530) (#623)
    * bump example versions to 2.23.0 and add changelog entry
    
    * Update changelog entry
    
    * Update changelog entry
    
    * Update changelog entry (typo fix)

--- examples/configurations/connectors/manage-offsets-mongo-db-source-connector/main.tf
+++ examples/configurations/connectors/manage-offsets-mongo-db-source-connector/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     confluent = {
       source  = "confluentinc/confluent"
-      version = "2.22.0"
+      version = "2.23.0"
     }
   }
 }
--- examples/configurations/private-link-schema-registry/main.tf
+++ examples/configurations/private-link-schema-registry/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
     confluent = {
       source  = "confluentinc/confluent"
-      version = "2.22.0"
+      version = "2.23.0"
     }
   }
 }
