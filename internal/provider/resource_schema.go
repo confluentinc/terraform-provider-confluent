9d384bc48b92db722ab32aecda4c62109e9f8aa7
Tushar Malik <tmalik@confluent.io>
2025-04-21 17:09:25 -0500

    Add comments

--- internal/provider/resource_schema.go
+++ internal/provider/resource_schema.go
@@ -352,11 +352,11 @@ func SetSchemaDiff(ctx context.Context, diff *schema.ResourceDiff, meta interfac
 
 	oldRuleset, newRuleset := diff.GetChange(paramRuleset)
 	if tfRuleset := diff.Get(paramRuleset).([]interface{}); len(tfRuleset) == 1 {
-		if len(newRuleset.([]interface{})) == 0 && len(oldRuleset.([]interface{})) > 0 {
+		if len(newRuleset.([]interface{})) == 0 && len(oldRuleset.([]interface{})) > 0 { //this is the case of an advanced package user trying to delete a ruleset.
 			ruleset := sr.NewRuleSet()
 			ruleset.SetDomainRules([]sr.Rule{})
 			createSchemaRequest.SetRuleSet(*ruleset)
-		} else {
+		} else { //this is the case of adding a ruleset.
 			ruleset := sr.NewRuleSet()
 			tfRulesetMap := tfRuleset[0].(map[string]interface{})
 			if tfRulesetMap[paramDomainRules] != nil {
