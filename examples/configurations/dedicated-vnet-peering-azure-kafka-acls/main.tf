2c09cf49465829b2a20712d985291863219e11bd
Channing Dong <165202380+channingdong@users.noreply.github.com>
2025-05-05 14:21:32 -0700

    #minor: prepare for v2.26.0 minor release (EXP-16368) (#670)
    * #minor: prepare for v2.26.0 minor release (EXP-16368)
    
    * Slight docs update
    
    * Address the PR comments

--- examples/configurations/dedicated-vnet-peering-azure-kafka-acls/main.tf
+++ examples/configurations/dedicated-vnet-peering-azure-kafka-acls/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     confluent = {
       source  = "confluentinc/confluent"
-      version = "2.25.0"
+      version = "2.26.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
