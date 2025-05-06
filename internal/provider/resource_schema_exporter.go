e5029b5212e219f9874ea57375c401e6f7fec8ce
Kostya Linou <klinou@confluent.io>
2025-05-06 11:54:49 -0700

    add status.GetState() == stateStarting

--- internal/provider/resource_schema_exporter.go
+++ internal/provider/resource_schema_exporter.go
@@ -47,6 +47,8 @@ const (
 	basicAuthUserInfoConfig               = "basic.auth.user.info"
 
 	schemaExporterAPICreateTimeout = 4 * time.Hour
+
+	stateStarting = "STARTING"
 )
 
 var standardConfigs = []string{basicAuthUserInfoConfig, schemaRegistryUrlConfig, basicAuthCredentialsSourceConfig}
@@ -289,7 +291,7 @@ func readSchemaExporterAndSetAttributes(ctx context.Context, d *schema.ResourceD
 	if err != nil {
 		return nil, fmt.Errorf("error creating Schema Exporter Status: %s", createDescriptiveError(err))
 	}
-	if status.GetState() == stateRunning {
+	if status.GetState() == stateRunning || status.GetState() == stateStarting {
 		if err := d.Set(paramStatus, stateRunning); err != nil {
 			return nil, err
 		}
