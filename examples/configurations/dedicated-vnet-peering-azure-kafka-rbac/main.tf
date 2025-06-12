0fbd1028baa095b8a419ca0b8025fcd3fa68ee58
tmalik <167480759+tmalikconfluent@users.noreply.github.com>
2025-06-12 17:36:43 -0500

    #minor: prepare for v2.32.0 minor release (#716)
    * #minor:Prepare for release version release-v2.23.0
    
    * nit
    
    * nit

--- examples/configurations/dedicated-vnet-peering-azure-kafka-rbac/main.tf
+++ examples/configurations/dedicated-vnet-peering-azure-kafka-rbac/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     confluent = {
       source  = "confluentinc/confluent"
-      version = "2.31.0"
+      version = "2.32.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
