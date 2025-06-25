3a975f38356542bfedd0c1e5bbd43ae5d0f0833f
Tushar Malik <tmalik@confluent.io>
2025-06-25 15:56:56 -0500

    Code changes

--- internal/provider/factory_utils.go
+++ internal/provider/factory_utils.go
@@ -237,10 +237,17 @@ func (f RetryableClientFactory) CreateRetryableClient() *http.Client {
 	return retryClient.StandardClient()
 }
 
-func customErrorHandler(resp *http.Response, err error, _ int) (*http.Response, error) {
+func customErrorHandler(resp *http.Response, err error, retries int) (*http.Response, error) {
 	if resp != nil {
+		body := resp.Body
+		defer body.Close()
 		if resp.StatusCode == 429 {
-			return resp, fmt.Errorf("received HTTP 429 Too Many Requests: (URL: %s, Method: %s)", resp.Request.URL, resp.Request.Method)
+			if err == nil {
+				return resp, fmt.Errorf("received HTTP 429 Too Many Requests: (URL: %s, Method: %s, Retries: %d)", resp.Request.URL, resp.Request.Method, retries)
+			} else {
+				return resp, fmt.Errorf("received HTTP 429 Too Many Requests: %v (URL: %s, Method: %s, Retries: %d)", err, resp.Request.URL, resp.Request.Method, retries)
+
+			}
 		}
 	}
 	return resp, err
