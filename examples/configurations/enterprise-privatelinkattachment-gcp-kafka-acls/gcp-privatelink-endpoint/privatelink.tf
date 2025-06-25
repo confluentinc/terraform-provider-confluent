2241f00eb68ef131e97b9aa3efbe97e5afdaec7c
Kostya Linou <klinou@confluent.io>
2025-06-25 11:36:32 -0700

    update vars

--- examples/configurations/enterprise-privatelinkattachment-gcp-kafka-acls/gcp-privatelink-endpoint/privatelink.tf
+++ examples/configurations/enterprise-privatelinkattachment-gcp-kafka-acls/gcp-privatelink-endpoint/privatelink.tf
@@ -1,101 +1,81 @@
-data "aws_vpc" "privatelink" {
-  id = var.vpc_id
-}
-
-data "aws_availability_zone" "privatelink" {
-  for_each = var.subnets_to_privatelink
-  zone_id = each.key
-}
-
 locals {
-  network_id = split(".", var.dns_domain)[0]
+  dns_domain = confluent_network.private-service-connect.dns_domain
+  network_id = split(".", local.dns_domain)[0]
 }
 
-resource "aws_security_group" "privatelink" {
-  # Ensure that SG is unique, so that this module can be used multiple times within a single VPC
-  name = "ccloud-privatelink_${local.network_id}_${var.vpc_id}"
-  description = "Confluent Cloud Private Link minimal security group for ${var.dns_domain} in ${var.vpc_id}"
-  vpc_id = data.aws_vpc.privatelink.id
-
-  ingress {
-    # only necessary if redirect support from http/https is desired
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = [data.aws_vpc.privatelink.cidr_block]
-  }
-
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = [data.aws_vpc.privatelink.cidr_block]
-  }
-
-  ingress {
-    from_port = 9092
-    to_port = 9092
-    protocol = "tcp"
-    cidr_blocks = [data.aws_vpc.privatelink.cidr_block]
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
+data "google_compute_network" "psc_endpoint_network" {
+  name = var.customer_vpc_network
 }
 
-resource "aws_vpc_endpoint" "privatelink" {
-  vpc_id = data.aws_vpc.privatelink.id
-  service_name = var.privatelink_service_name
-  vpc_endpoint_type = "Interface"
-
-  security_group_ids = [
-    aws_security_group.privatelink.id,
-  ]
-
-  subnet_ids = [for zone, subnet_id in var.subnets_to_privatelink: subnet_id]
-  private_dns_enabled = false
+data "google_compute_subnetwork" "psc_endpoint_subnetwork" {
+  name = var.customer_subnetwork_name
 }
 
-resource "aws_route53_zone" "privatelink" {
-  name = var.dns_domain
+resource "google_compute_address" "psc_endpoint_ip" {
+  for_each = var.subnet_name_by_zone
 
-  vpc {
-    vpc_id = data.aws_vpc.privatelink.id
+  name         = "ccloud-endpoint-ip-${local.network_id}-${each.key}"
+  subnetwork   = var.customer_subnetwork_name
+  address_type = "INTERNAL"
+}
+
+# Private Service Connect endpoint
+resource "google_compute_forwarding_rule" "psc_endpoint_ilb" {
+  for_each = var.subnet_name_by_zone
+
+  name = "ccloud-endpoint-${local.network_id}-${each.key}"
+
+  target                = lookup(confluent_network.private-service-connect.gcp[0].private_service_connect_service_attachments, each.key, "\n\nerror: ${each.key} subnet is missing from CCN's Private Service Connect service attachments")
+  load_balancing_scheme = "" # need to override EXTERNAL default when target is a service attachment
+  network               = var.customer_vpc_network
+  ip_address            = google_compute_address.psc_endpoint_ip[each.key].id
+}
+
+# Private hosted zone for Private Service Connect endpoints
+resource "google_dns_managed_zone" "psc_endpoint_hz" {
+  name     = "ccloud-endpoint-zone-${local.network_id}"
+  dns_name = "${local.dns_domain}."
+
+  visibility = "private"
+
+  private_visibility_config {
+    networks {
+      network_url = data.google_compute_network.psc_endpoint_network.id
+    }
   }
 }
 
-resource "aws_route53_record" "privatelink" {
-  count = length(var.subnets_to_privatelink) == 1 ? 0 : 1
-  zone_id = aws_route53_zone.privatelink.zone_id
-  name = "*.${aws_route53_zone.privatelink.name}"
-  type = "CNAME"
-  ttl  = "60"
-  records = [
-    aws_vpc_endpoint.privatelink.dns_entry[0]["dns_name"]
+resource "google_dns_record_set" "psc_endpoint_rs" {
+  name = "*.${google_dns_managed_zone.psc_endpoint_hz.dns_name}"
+  type = "A"
+  ttl  = 60
+
+  managed_zone = google_dns_managed_zone.psc_endpoint_hz.name
+  rrdatas = [
+    for zone, _ in var.subnet_name_by_zone : google_compute_address.psc_endpoint_ip[zone].address
   ]
 }
 
-locals {
-  endpoint_prefix = split(".", aws_vpc_endpoint.privatelink.dns_entry[0]["dns_name"])[0]
+resource "google_dns_record_set" "psc_endpoint_zonal_rs" {
+  for_each = var.subnet_name_by_zone
+
+  name = "*.${each.key}.${google_dns_managed_zone.psc_endpoint_hz.dns_name}"
+  type = "A"
+  ttl  = 60
+
+  managed_zone = google_dns_managed_zone.psc_endpoint_hz.name
+  rrdatas      = [google_compute_address.psc_endpoint_ip[each.key].address]
 }
 
-resource "aws_route53_record" "privatelink-zonal" {
-  for_each = var.subnets_to_privatelink
+resource "google_compute_firewall" "allow-https-kafka" {
+  name    = "ccloud-endpoint-firewall-${local.network_id}"
+  network = data.google_compute_network.psc_endpoint_network.id
 
-  zone_id = aws_route53_zone.privatelink.zone_id
-  name = length(var.subnets_to_privatelink) == 1 ? "*" : "*.${each.key}"
-  type = "CNAME"
-  ttl  = "60"
-  records = [
-    format("%s-%s%s",
-      local.endpoint_prefix,
-      data.aws_availability_zone.privatelink[each.key].name,
-      replace(aws_vpc_endpoint.privatelink.dns_entry[0]["dns_name"], local.endpoint_prefix, "")
-    )
-  ]
-}
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "9092"]
+  }
 
-output "vpc_endpoint_id" {
-  value = aws_vpc_endpoint.privatelink.id
+  direction          = "EGRESS"
+  destination_ranges = [data.google_compute_subnetwork.psc_endpoint_subnetwork.ip_cidr_range]
 }
--- examples/configurations/enterprise-privatelinkattachment-gcp-kafka-acls/gcp-privatelink-endpoint/variables.tf
+++ examples/configurations/enterprise-privatelinkattachment-gcp-kafka-acls/gcp-privatelink-endpoint/variables.tf
@@ -1,19 +1,35 @@
-variable "vpc_id" {
-  description = "The VPC ID to private link to Confluent Cloud"
+variable "confluent_cloud_api_key" {
+  description = "Confluent Cloud API Key (also referred as Cloud API ID)"
   type        = string
 }
 
-variable "privatelink_service_name" {
-  description = "The Service Name from Confluent Cloud to Private Link with (provided by Confluent)"
+variable "confluent_cloud_api_secret" {
+  description = "Confluent Cloud API Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "customer_project_id" {
+  description = "The GCP Project ID"
   type        = string
 }
 
-variable "dns_domain" {
-  description = "The root DNS domain for the Private Link Attachment, for example, `pr123a.us-east-2.aws.confluent.cloud`"
+variable "region" {
+  description = "The region of Confluent Cloud Network"
   type        = string
 }
 
-variable "subnets_to_privatelink" {
-  description = "A map of Zone ID to Subnet ID (ie: {\"use1-az1\" = \"subnet-abcdef0123456789a\", ...})"
+variable "customer_vpc_network" {
+  description = "The VPC network name to provision Private Service Connect endpoint to Confluent Cloud"
+  type        = string
+}
+
+variable "customer_subnetwork_name" {
+  description = "The subnetwork name to provision Private Service Connect endpoint to Confluent Cloud"
+  type        = string
+}
+
+variable "subnet_name_by_zone" {
+  description = "A map of Zone to Subnet Name"
   type        = map(string)
 }
