df470f242cadb0eb98f4e6c19cb2bb8a8e000c7f
Kostya Linou <klinou@confluent.io>
2025-07-29 16:41:31 -0700

    Update Enterprise PNI example to use v2.36.0.

--- examples/configurations/enterprise-pni-aws-kafka-rbac/main.tf
+++ examples/configurations/enterprise-pni-aws-kafka-rbac/main.tf
@@ -7,7 +7,7 @@ terraform {
     }
     confluent = {
       source  = "confluentinc/confluent"
-      version = "2.32.0"
+      version = "2.36.0"
     }
     random = {
       source  = "hashicorp/random"
