2ef4eb96ff499fa3bc6124a4bd33162f7aada748
Channing Dong <165202380+channingdong@users.noreply.github.com>
2025-07-29 17:11:16 -0700

    Documentation update to support Microsoft Entra ID as identity provider

---
+++ examples/configurations/authentication-using-oauth/okta/variables.tf
@@ -0,0 +1,20 @@
+variable "oauth_external_token_url" {
+  description = "The OAuth token URL from Okta as the identity provider"
+  type        = string
+}
+
+variable "oauth_external_client_id" {
+  description = "The OAuth token client id from Okta as the identity provider"
+  type        = string
+}
+
+variable "oauth_external_client_secret" {
+  description = "The OAuth token client secret from Okta as the identity provider"
+  type        = string
+  sensitive   = true
+}
+
+variable "oauth_identity_pool_id" {
+  description = "The OAuth identity pool id from Okta as the identity provider, registered with Confluent Cloud"
+  type        = string
+}
