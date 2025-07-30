656add9bc46dd6f9000f47439aa0dad2d9be98e2
Cynthia Qin <cqin@confluent.io>
2025-07-30 13:23:50 -0700

    Add diffsupress func

--- internal/provider/resource_kafka_topic_test.go
+++ internal/provider/resource_kafka_topic_test.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 const (
