6b5479002c1060e6e93210b8192f398f64ecbfdc
Kostya Linou <klinou@confluent.io>
2025-08-05 13:31:25 -0700

    add disable_wait_for_ready = true

--- examples/configurations/dedicated-privatelink-aws-kafka-rbac-nginx/main.tf
+++ examples/configurations/dedicated-privatelink-aws-kafka-rbac-nginx/main.tf
@@ -395,13 +395,13 @@ resource "confluent_api_key" "app-producer-kafka-api-key" {
 resource "confluent_role_binding" "app-producer-developer-write" {
   principal   = "User:${confluent_service_account.app-producer.id}"
   role_name   = "DeveloperWrite"
-  crn_pattern = "${confluent_kafka_cluster.dedicated.rbac_crn}/kafka=${confluent_kafka_cluster.dedicated.id}/topic=${confluent_kafka_topic.orders.topic_name}"
+  crn_pattern = "${confluent_kafka_cluster.dedicated.rbac_crn}/kafka=${confluent_kafka_cluster.dedicated.id}/topic=${local.topic_name}"
 }
 
 resource "confluent_role_binding" "app-consumer-developer-read-from-topic" {
   principal   = "User:${confluent_service_account.app-consumer.id}"
   role_name   = "DeveloperRead"
-  crn_pattern = "${confluent_kafka_cluster.dedicated.rbac_crn}/kafka=${confluent_kafka_cluster.dedicated.id}/topic=${confluent_kafka_topic.orders.topic_name}"
+  crn_pattern = "${confluent_kafka_cluster.dedicated.rbac_crn}/kafka=${confluent_kafka_cluster.dedicated.id}/topic=${local.topic_name}"
 }
 
 resource "confluent_role_binding" "app-consumer-developer-read-from-group" {
