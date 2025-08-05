6e2c4dff20bbe24686cd1f757e864bd2a1bb8bde
Kostya Linou <klinou@confluent.io>
2025-08-05 13:26:28 -0700

    add disable_wait_for_ready = true

--- examples/configurations/dedicated-privatelink-aws-kafka-rbac-nginx/main.tf
+++ examples/configurations/dedicated-privatelink-aws-kafka-rbac-nginx/main.tf
@@ -300,7 +300,7 @@ resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
 resource "confluent_api_key" "app-manager-kafka-api-key" {
   display_name = "app-manager-kafka-api-key"
   description  = "Kafka API Key that is owned by 'app-manager' service account"
-
+  disable_wait_for_ready = true
   owner {
     id          = confluent_service_account.app-manager.id
     api_version = confluent_service_account.app-manager.api_version
@@ -346,7 +346,7 @@ resource "confluent_service_account" "app-consumer" {
 resource "confluent_api_key" "app-consumer-kafka-api-key" {
   display_name = "app-consumer-kafka-api-key"
   description  = "Kafka API Key that is owned by 'app-consumer' service account"
-
+  disable_wait_for_ready = true
   owner {
     id          = confluent_service_account.app-consumer.id
     api_version = confluent_service_account.app-consumer.api_version
@@ -379,7 +379,7 @@ resource "confluent_service_account" "app-producer" {
 resource "confluent_api_key" "app-producer-kafka-api-key" {
   display_name = "app-producer-kafka-api-key"
   description  = "Kafka API Key that is owned by 'app-producer' service account"
-
+  disable_wait_for_ready = true
   owner {
     id          = confluent_service_account.app-producer.id
     api_version = confluent_service_account.app-producer.api_version
