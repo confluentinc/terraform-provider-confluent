b7e0f8dc218b6ac597c5be513ce14c9885ee99fa
Kostya Linou <klinou@confluent.io>
2025-09-29 15:47:32 -0700

    update CHANGELOG.md

--- examples/configurations/dedicated-vpc-peering-aws-kafka-acls/main.tf
+++ examples/configurations/dedicated-vpc-peering-aws-kafka-acls/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     confluent = {
       source  = "confluentinc/confluent"
-      version = "2.42.0"
+      version = "2.43.0"
     }
     aws = {
       source  = "hashicorp/aws"
