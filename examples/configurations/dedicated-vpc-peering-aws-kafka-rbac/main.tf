7a210a62aab223aafb74cf66be7cebcb6eea237c
Daniel Ayaz <dayaz@confluent.io>
2025-10-15 23:37:11 -0500

    #minor Prepare for 2.50.0 release (#871)
    * Reapply "Revert provider integration v2 (#856)" (#865)
    
    This reverts commit 5b6b24afb890d7eb93b83aa49313604144bf90aa.
    
    * Revert "APIE-415: Support in-place update for the recreate_on_update attribute of the confluent_schema resource (#863)"
    
    This reverts commit 708134dce78006887dd3715a487e3f0c6dd8cd9c.
    
    * #minor Prepare for 2.50.0 release

--- examples/configurations/dedicated-vpc-peering-aws-kafka-rbac/main.tf
+++ examples/configurations/dedicated-vpc-peering-aws-kafka-rbac/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     confluent = {
       source  = "confluentinc/confluent"
-      version = "2.49.0"
+      version = "2.50.0"
     }
     aws = {
       source  = "hashicorp/aws"
