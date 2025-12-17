bfe3f75b49ff9472caacd2e46b5f7c37361945f8
Kostya Linou <klinou@confluent.io>
2025-12-17 13:23:13 -0800

    update TestAccKafkaClusterDataSourceLive

--- internal/provider/data_source_kafka_cluster_live_test.go
+++ internal/provider/data_source_kafka_cluster_live_test.go
@@ -72,7 +72,7 @@ func TestAccKafkaClusterDataSourceLive(t *testing.T) {
 					resource.TestCheckResourceAttrSet(fmt.Sprintf("data.confluent_kafka_cluster.%s", kafkaClusterDataSourceLabel), "bootstrap_endpoint"),
 					resource.TestCheckResourceAttrSet(fmt.Sprintf("data.confluent_kafka_cluster.%s", kafkaClusterDataSourceLabel), "rest_endpoint"),
 					resource.TestCheckResourceAttrSet(fmt.Sprintf("data.confluent_kafka_cluster.%s", kafkaClusterDataSourceLabel), "rest_endpoint"),
-					resource.TestCheckResourceAttr(kafkaClusterDataSourceLabel, "standard.0.max_ecku", "10"),
+					resource.TestCheckResourceAttr(fmt.Sprintf("data.confluent_kafka_cluster.%s", kafkaClusterDataSourceLabel), "standard.0.max_ecku", "10"),
 				),
 			},
 		},
