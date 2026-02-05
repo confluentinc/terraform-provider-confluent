4cb4e7d0335826ccb0de77d24990d362003a6c8f
Steven Gagniere <sgagniere@confluent.io>
2026-02-05 13:34:12 -0800

    Revert hashicorp/azurerm version back to 2.55.0 in the examples

--- examples/configurations/dedicated-vnet-peering-azure-kafka-acls/main.tf
+++ examples/configurations/dedicated-vnet-peering-azure-kafka-acls/main.tf
@@ -7,7 +7,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.61.0"
+      version = "~> 2.55.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
