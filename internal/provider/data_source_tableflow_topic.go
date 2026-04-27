38157ca260849e26f5a946d16fbc9b41e944ac94
Alex Sorokoumov <aleksandr.sorokoumov@gmail.com>
2026-04-27 16:30:51 -0700

    USTORAGE-11250 Add data_retention_ms support to confluent_tableflow_topic resource
    Support the data_retention_ms parameter for controlling max age of data
    records in Tableflow-enabled topics (min 30 days / 2592000000ms). Upgrade
    tableflow SDK from v0.5.0 to v0.6.0 to get the new field.
    
    Changes span schema definition, CRUD operations, data source, mock test
    fixtures, acceptance tests, live integration tests, and documentation.

--- internal/provider/data_source_tableflow_topic.go
+++ internal/provider/data_source_tableflow_topic.go
@@ -53,6 +53,11 @@ func tableflowTopicDataSource() *schema.Resource {
 				Computed:    true,
 				Description: "Retention time in milliseconds for the Tableflow enabled topic.",
 			},
+			paramDataRetentionMs: {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The maximum age, in milliseconds, of data to retain in the table for the Tableflow-enabled topic.",
+			},
 			paramTableFormats: {
 				Type:        schema.TypeSet,
 				Computed:    true,
