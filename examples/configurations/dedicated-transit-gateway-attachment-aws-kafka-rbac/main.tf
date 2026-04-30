345e12dd5ca79c7932491b07c1dbde889c7a4986
Daniel Ayaz <dayaz@confluent.io>
2026-04-30 13:54:21 -0500

    #minor Prepare for 2.71.0 release

--- examples/configurations/dedicated-transit-gateway-attachment-aws-kafka-rbac/main.tf
+++ examples/configurations/dedicated-transit-gateway-attachment-aws-kafka-rbac/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     confluent = {
       source  = "confluentinc/confluent"
-      version = "2.70.0"
+      version = "2.71.0"
     }
     aws = {
       source  = "hashicorp/aws"
