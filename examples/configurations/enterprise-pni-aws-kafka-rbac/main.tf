ea7ee34b8ce472cb9c5807aaecfdc8743c21a551
Channing Dong <165202380+channingdong@users.noreply.github.com>
2026-07-09 11:05:16 -0700

    #minor Prepare for 2.77.0 release (#1045)

--- examples/configurations/enterprise-pni-aws-kafka-rbac/main.tf
+++ examples/configurations/enterprise-pni-aws-kafka-rbac/main.tf
@@ -7,7 +7,7 @@ terraform {
     }
     confluent = {
       source  = "confluentinc/confluent"
-      version = "2.76.0"
+      version = "2.77.0"
     }
     random = {
       source  = "hashicorp/random"
