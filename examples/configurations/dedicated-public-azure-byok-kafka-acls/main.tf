8d2332617f5ba60a2df854cc5e7063ed2e6c25d2
Channing Dong <cdong@confluent.io>
2026-09-23 13:49:07 -0700

    minor: prepare for v2.87.0 release

--- examples/configurations/dedicated-public-azure-byok-kafka-acls/main.tf
+++ examples/configurations/dedicated-public-azure-byok-kafka-acls/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     confluent = {
       source  = "confluentinc/confluent"
-      version = "2.86.0"
+      version = "2.87.0"
     }
 
     azurerm = {
--- examples/configurations/enterprise-pni-aws-kafka-rbac/main.tf
+++ examples/configurations/enterprise-pni-aws-kafka-rbac/main.tf
@@ -7,7 +7,7 @@ terraform {
     }
     confluent = {
       source  = "confluentinc/confluent"
-      version = "2.86.0"
+      version = "2.87.0"
     }
     random = {
       source  = "hashicorp/random"
